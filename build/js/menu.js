c40f08283b1df5e4928b3d035e5bd0dd42aa120a
LisaVaselisa <deoreczk@list.ru>
2021-03-16 05:20:57 +0300

    прошлась по критериям, пп первых двух блоков

---
+++ build/js/menu.js
@@ -0,0 +1,10 @@
+
+// 'use strict';
+// var navList = document.querySelector('.nav__list');
+// var navToggle = document.querySelector('.nav__toggle');
+
+
+// navList.classList.remove('nav__list--nojs');
+// navToggle.addEventListener('click', function() {
+//   navList.classList.toggle('list--open');
+// });

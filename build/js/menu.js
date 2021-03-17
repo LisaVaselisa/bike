61c476845aac76d48c2f90ff328458bf8e8fafe1
LisaVaselisa <deoreczk@list.ru>
2021-03-17 04:46:13 +0300

    ПП почти закончен и частично реализован js меню

--- build/js/menu.js
+++ build/js/menu.js
@@ -1,10 +1,13 @@
 
-// 'use strict';
-// var navList = document.querySelector('.nav__list');
-// var navToggle = document.querySelector('.nav__toggle');
+'use strict';
+
+// Меню
+
+var navList = document.querySelector('.nav__list');
+var navToggle = document.querySelector('.nav__toggle');
 
 
-// navList.classList.remove('nav__list--nojs');
-// navToggle.addEventListener('click', function() {
-//   navList.classList.toggle('list--open');
-// });
+navList.classList.remove('nav__list--nojs');
+navToggle.addEventListener('click', function() {
+  navList.classList.toggle('nav__list--close');
+});

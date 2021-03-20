57907b9a06958d9e9998d0ec397a237a2d7f48ab
LisaVaselisa <deoreczk@list.ru>
2021-03-20 19:51:04 +0300

    маска на тел

--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,6 @@
 'use strict';
+
+// Меню
 var menuNav = document.querySelector('.nav');
 var navToggle = document.querySelector('.nav__toggle');
 var body = document.querySelector('.body');
@@ -26,3 +28,43 @@ if (menuNav && navToggle) {
 
   changeMenuPosition();
 }
+
+// Маска для формы
+var maskedInputs = document.querySelectorAll("[data-mask]");
+
+for (var index = 0; index < maskedInputs.length; index++) {
+  maskedInputs[index].addEventListener('input', maskInput);
+}
+
+function maskInput() {
+  var input = this;
+  var mask = input.dataset.mask;
+  var value = input.value;
+  var literalPattern = /[0\*]/;
+  var numberPattern = /[0-9]/;
+  var newValue = "";
+  try {
+    var maskLength = mask.length;
+    var valueIndex = 0;
+    var maskIndex = 0;
+
+    for (; maskIndex < maskLength;) {
+      if (maskIndex >= value.length) break;
+
+      if (mask[maskIndex] === "0" && value[valueIndex].match(numberPattern) === null) break;
+
+      while (mask[maskIndex].match(literalPattern) === null) {
+        if (value[valueIndex] === mask[maskIndex]) break;
+        newValue += mask[maskIndex++];
+      }
+      newValue += value[valueIndex++];
+      maskIndex++;
+    }
+
+    input.value = newValue;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+
